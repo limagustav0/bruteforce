7d48402418b82ad7c4e812b0c0701c5cc4ef28d9
Gustavo C. Lima <87615776+limagustav0@users.noreply.github.com>
2022-03-24 19:52:03 -0300

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -2,14 +2,22 @@ import random
 
 def quebraSenha(num):
     try:
+        #Transforma a senha numérica digitada numa lista de string
         senha = list(str(num))
 
+        #Cria uma sequência de numeros aleatórios de acordo com a quantidade de senhas digitadas acima
         quebraSenha = [random.randint(0,9) for num in range(0,len(senha))]
 
+        #Contador para somar o tanto de tentativas
         contador = 0
+
+        #Transforma a senha digitada numa lista de dados do tipo INT
         listaSenha = [int(num) for num in senha]
+
+        #Loop while para encontrar a senha digitada
         while listaSenha != quebraSenha:
             contador += 1
+            #Quando a senha contida na listaSenha não é encontrado pelo quebraSenha, o conjunto de dados é aleatóriamente redefinido
             quebraSenha = [random.randint(0, 9) for num in range(0, len(senha))]
             print(quebraSenha)
         else:
@@ -19,4 +27,3 @@ def quebraSenha(num):
 
 if __name__ == '__main__':
     quebraSenha(123)
-    
